6cd224eacf1698a2f737fb5a3f3f076549ce2ee8
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-04-13 01:27:18 +0200

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/typing-speed-test/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 });
