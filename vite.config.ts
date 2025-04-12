9c64af0550af9f2e52e4079a089a8869b94ea591
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-04-13 00:33:19 +0200

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/typing-speed-test/", // Ensures correct paths on GitHub Pages
-  plugins: [react()]
-})
+  base: '/typing-speed-test/',
+  plugins: [react()],
+});
