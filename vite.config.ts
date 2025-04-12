a1e3bb7194064a3906807a72f4c0468c8892bd2f
Dolfie <35562093+njvanas@users.noreply.github.com>
2025-04-12 23:41:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/njvanas.github.io/typing-speed-test/', // Correct path for GitHub Pages subpath
+  base: '/typing-speed-test/', // Correct path for GitHub Pages subpath
   plugins: [react()],
   build: {
     outDir: 'dist',  // Output directory for production build
